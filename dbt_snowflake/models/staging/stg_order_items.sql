39577217c903cd2e5172d8f9062c67e365c65d69
Sharaf Ahmed <sharafahmed2002@gmail.com>
2025-05-02 19:19:04 +0300

    Update staging

--- dbt_snowflake/models/staging/stg_order_items.sql
+++ dbt_snowflake/models/staging/stg_order_items.sql
@@ -3,5 +3,5 @@ SELECT id item_id,
         product_id,
         quantity,
         unit_price,
-        (quantity * unit_price) total_price
+        {{ total_price('unit_price', 'quantity') }} total_price
 FROM {{ source('raw_data', 'order_items') }}
--- dbt_snowflake/models/staging/stg_orders.sql
+++ dbt_snowflake/models/staging/stg_orders.sql
@@ -1,5 +1,6 @@
 SELECT id order_id,
         customer_id,
         order_date,
+        {{ classify_quarter('order_date') }} AS quarter,
         status
 FROM {{ source('raw_data', 'orders') }}
